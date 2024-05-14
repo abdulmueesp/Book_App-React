1ccad3bae130e0ecd13f37564aaf51ef7fc74f9f
abdulmueesp <abdulmueesp5@gmail.com>
2024-05-15 00:46:09 +0530

    Footer completed

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import instagram from '../assets/instagram.png'
+import linkedin from '../assets/linkedin.png'
+import pinterest from "../assets/pinterest.png"
+import youtube from "../assets/youtube.png"
+import twitter from "../assets/twitter.png"
 
 const Footer = () => {
+  const medias=[instagram,linkedin,pinterest,youtube,twitter]
   return (
-    <div className='w-full h-96 bg-gray-600 mt-8 flex flex-col '>
+    <div className='w-full h-min  mt-8 flex flex-col border-t-2 border-gray-600 mb-6 '>
         {/* about  */}
-      <div className='w-full h-60 bg-white border border-red-500 flex justify-evenly mt-12'>
-      <div className='w-60 h-44 bg-gray-300 flex flex-col items-center space-y-1'>
+      <div className='w-full h-min bg-white  flex justify-evenly mt-12'>
+      <div className='w-60 h-44  flex flex-col items-center space-y-1'>
          <h1 className='text-lg font-semibold italic text-red-700'>Company</h1>
          <h3 className={`footer_txt`}>About Us</h3>
          <h3 className={`footer_txt`}>Career</h3>
@@ -13,7 +19,7 @@ const Footer = () => {
          <h3 className={`footer_txt`}>Contact Us</h3>
       </div>
 
-      <div className='w-60 h-44 bg-gray-300 flex flex-col items-center space-y-1'>
+      <div className='w-60 h-44  flex flex-col items-center space-y-1'>
          <h1 className='text-lg font-semibold italic text-red-700'>Policies</h1>
          <h3 className={`footer_txt`}>Privacy Policies</h3>
          <h3 className={`footer_txt`}>Terms of Use</h3>
@@ -21,25 +27,36 @@ const Footer = () => {
          <h3 className={`footer_txt`}>Copyright Policy</h3>
       </div>
 
-      <div className='w-60 h-44 bg-gray-300 flex flex-col items-center space-y-1'>
+      <div className='w-60 h-44  flex flex-col items-center space-y-1'>
          <h1 className='text-lg font-semibold italic text-red-700'>Help</h1>
          <h3 className={`footer_txt`}>Payment</h3>
          <h3 className={`footer_txt`}>Shipping</h3>
-         <h3 className={`footer_txt`}>Secure Shopping</h3>
-         <h3 className={`footer_txt`}>Copyright Policy</h3>
+         <h3 className={`footer_txt`}>Return</h3>
+         <h3 className={`footer_txt`}>FAQ</h3>
       </div>
 
-        <div className='w-60 h-44 bg-pink-600'>
-
-         </div>
+      <div className='w-60 h-44  flex flex-col items-center space-y-1'>
+         <h1 className='text-lg font-semibold italic text-red-700'>Misc</h1>
+         <h3 className={`footer_txt`}>Affiliate</h3>
+         <h3 className={`footer_txt`}>Sitemap</h3>     
+      </div>
 
 
       </div>
         {/* icons  */}
-       <div></div>
+
+       <div className='w-full h-14  flex justify-center items-center space-x-2'>
+         {medias.map((media,index)=>(
+         <img src={media} alt="" className='w-8 h-8' />
+         ))}
+        
+
+       </div>
 
        {/* copr right  */}
-       <div></div>
+       <div className='w-full h-7  flex justify-center'>
+      <h1 className='text-gray-400 text-lg'>Copyright © 2024 .BookPalace.com. All Rights Reserved</h1> 
+       </div>
        
 
     </div>
